8a76989a79a6720d5cfb6f38f106b36cc392e27d
MJTRK01 <mjidutarik@gmail.com>
2025-03-20 22:26:27 +0000

    Added Command-Line Arguments

--- PortScanner.py
+++ PortScanner.py
@@ -1,4 +1,5 @@
 import socket
+import argparse
 
 
 def port_scan(target_ip, start_port, end_port):
@@ -10,5 +11,17 @@ def port_scan(target_ip, start_port, end_port):
             print(f"Port {port} is open")
         sock.close()
 
-target_ip = "10.10.X.X" #Change This
-port_scan(target_ip,1, 1000)
+
+if __name__ == "__main__":
+    # Set up command line argument parsing
+    parser = argparse.ArgumentParser(description="Port scanner Tool")
+    parser.add_argument("target_ip",type=str, help="Target IP address to scan")
+    parser.add_argument("start_port",type=int, help="Start port of the scan range")
+    parser.add_argument("end_port",type=int, help="End port of the the scan range")
+
+    args = parser.parse_args() #Parse the arguments
+
+    port_scan(args.target_ip, args.start_port, args.end_port)
+
+#target_ip = "10.10.X.X" #Change This
+#port_scan(target_ip,1, 1000)
